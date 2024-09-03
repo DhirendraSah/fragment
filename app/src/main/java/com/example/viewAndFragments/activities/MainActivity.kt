40e6bf3474fe771e5a1af6651543074cee6392b6
DhirendraSah <157945366+DhirendraSah@users.noreply.github.com>
2024-09-03 09:26:56 +0530

    commit

---
+++ app/src/main/java/com/example/viewAndFragments/activities/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.viewAndFragments.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+
+    }
+}
